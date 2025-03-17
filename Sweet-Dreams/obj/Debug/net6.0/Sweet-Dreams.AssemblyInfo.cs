4225b07778fa7f0ee6f2cbbec71b7912da13d948
bug-type-trainer <ajk5756@rit.edu>
2025-03-17 14:09:28 -0400

    Updates to json, cache, and config files.

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776e477b5e5498137d5f5e572303bf5411300911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb20d72d01d5ab26239458ef30faa7288b389485")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
