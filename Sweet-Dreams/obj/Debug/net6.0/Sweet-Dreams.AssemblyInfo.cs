63910916bc0c45263eccaa35024abb5199336894
SD0249 <iamseoyoung05@gmail.com>
2025-03-17 13:42:13 -0400

    Constructing the LevelTile class
    A class to represent a single tile to form a level class. Then possibly a level manager.

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba8ad818c53314343d5af2fedf17a7eea005c4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1af973cd22a33cf097131cedfc0794fe68c308")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
