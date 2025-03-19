81d181e5296c4466d7bc3cc983578b9966c20e76
Amy Lee (RIT Student) <iamseoyoung05@gmail.com>
2025-03-19 18:22:54 -0400

    Commentary Update on classes
    Added the basic description of our game and our team name on each .cs files.

--- Sweet-Dreams/Bullet.cs
+++ Sweet-Dreams/Bullet.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Sweet Dreams - Sugar Rush
+// A shooter game. Kill all the enemies to survive and collect candies!
 namespace Sweet_Dreams
 {
     internal class Bullet
