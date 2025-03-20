6dc70b8845936b94d87dd5e8550cfed67fca237b
Pale-Stag <bm8963@g.rit.edu>
2025-03-19 20:14:46 -0400

    Files
    files

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6359ef6cc311b35856bba6bc18d3141dae363a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4754b875039838eeaee9b727f489666ef30ff3e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
