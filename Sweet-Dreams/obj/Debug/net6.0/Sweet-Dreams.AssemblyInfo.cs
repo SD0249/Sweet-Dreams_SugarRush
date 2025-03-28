79c8c06272e8fa0dc88d26e8bbacd0dd92758749
Pale-Stag <bm8963@g.rit.edu>
2025-03-28 13:33:48 -0400

    File pull
    Pulled files from the directory

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ca55205734571006800d6df4ff25873d5cba5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd9efb1773cf09a2c164b7c6460add866e38b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
