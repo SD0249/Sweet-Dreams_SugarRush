750de18dcfe83fa352dae2a6a0a3d110ef5bf87f
SD0249 <iamseoyoung05@gmail.com>
2025-03-31 13:03:49 -0400

    Assembly Information Update
    Working with the assembly information

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f96f6d7c0d30dff3ac50230afb9881eb3ce27a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c9468906599dff253abd146d5ed303001a7ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
