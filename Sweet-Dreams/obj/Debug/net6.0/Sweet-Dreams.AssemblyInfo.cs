9bcc969b31d16f5715a868889f76c39200d5d03a
Amy Lee (RIT Student) <iamseoyoung05@gmail.com>
2025-04-09 19:21:04 -0400

    Binary File Changes

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9737b82879db925744fba3c14d0d070bf93bda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926816ab5e18048ffe7c279d0c24a354f50353f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
