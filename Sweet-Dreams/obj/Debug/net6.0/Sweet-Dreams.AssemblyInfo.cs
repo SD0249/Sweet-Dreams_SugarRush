5ad42984749de6b1ae20d537e82b5367628f7f45
Pale-Stag <bm8963@g.rit.edu>
2025-04-11 22:29:26 -0400

    File change

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33a0f45e9b0c0102ba4a52297bd260ed267e605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d132fe618b9ff452c6684282cabbfb7c79e18501")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
