7438c17bf61a41001c790793dfff3739bff7ba65
SD0249 <iamseoyoung05@gmail.com>
2025-04-11 23:07:50 -0400

    File Changes

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d132fe618b9ff452c6684282cabbfb7c79e18501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d652804b0e9628088272d9187a96d089fd8b45f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
