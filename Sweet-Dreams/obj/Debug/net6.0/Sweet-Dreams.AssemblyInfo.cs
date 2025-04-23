33b66bebf90e325c5440dffcec67cab0a47ac70f
Pale-Stag <bm8963@g.rit.edu>
2025-04-23 18:55:48 -0400

    Shockwave added

--- Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
+++ Sweet-Dreams/obj/Debug/net6.0/Sweet-Dreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35c88b1f67b07339b4562c1c5f6b325dafeb6c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac95e11139707132e12a4e333ae8ef374f27c5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sweet-Dreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
